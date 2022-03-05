7ec9fc64748174d715cc65b1efb63e496593ed1e
Nikolay Forostianyi <forostianyi@mail.ru>
2022-03-05 11:54:01 +0500

    Autorization continuation

--- src/main/java/ru/gpb/school/moneyTransfer/repositories/TransferUserRepo.java
+++ src/main/java/ru/gpb/school/moneyTransfer/repositories/TransferUserRepo.java
@@ -4,10 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.gpb.school.moneyTransfer.model.TransferUser;
 
 import java.util.List;
-import java.util.Optional;
+
 
 public interface TransferUserRepo extends JpaRepository<TransferUser,String> {
-    Optional<TransferUser> findTransferUsersById(String Id);
     List<TransferUser> findAll();
     TransferUser findByUserName(String username);
 }
